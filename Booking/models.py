7a66aa785daf318f912fd1136f46a58d541e4a78
GHOST146 <30757055+GHOST146@users.noreply.github.com>
2024-03-25 16:56:23 +0800

    Delete Booking directory

--- Booking/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Bookingmodel(models.Model):
-    date = models.DateField(auto_now=False, auto_now_add=False)
-    email = models.EmailField(max_length=254)
-    address = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='PropertyImage/', max_length=100)
